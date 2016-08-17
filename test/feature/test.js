fd766a73513b9d0d0c58c3f133e733f2551174e8
Kris Foss <kris.foss@gmail.com>
2016-08-17 14:02:19 -0600

    WIP mid test

--- test/feature/test.js
+++ test/feature/test.js
@@ -1,18 +1,20 @@
-const assert =  require('assert')
+const assert =  require('assert');
 
 describe('welcome page', function(){
   it('should be able to grab the page title', function(){
   	browser.url('/');
-  	var title = browser.getTitle()
+  	var title = browser.getTitle();
   	assert.equal(title, 'Chat Adventures');
     });
+
   it('send button should be disabled if input field is empty', function() {
     browser.url('/');
     var messageInput =browser.element('#message-input');
     messageInput.setValue('');
     var buttonStatus = browser.getAttribute('#send-button', 'disabled');
-    assert.equal(buttonStatus, 'true')
-  })
+    assert.equal(buttonStatus, 'true');
+  });
+
   it('send button should be enabled if input field has a value', function() {
     browser.url('/');
     var messageInput =browser.element('#message-input');
@@ -20,17 +22,27 @@ describe('welcome page', function(){
     var buttonStatus = browser.getAttribute('#send-button', 'disabled');
     assert.equal(buttonStatus, null);
   });
+
   it('should clear input field on submit button click', function() {
     browser.url('/');
     var messageInput = browser.element('#message-input');
     messageInput.setValue('test');
     browser.click('#send-button');
+    var buttonStatus = browser.element('#send-button').value.ELEMENT
+    var buttonTrueStatus = browser.elementIdEnabled(buttonStatus).value
+
+    console.log(buttonStatus)
     assert.equal(messageInput.getAttribute('value'), '');
   });
-  // it('should put user messages in the DOM', function() {
-  //   browser.url('/');
-  //   var
-  // })
+
+  it('should put user messages in the DOM', function() {
+    browser.url('/');
+    var messageInput = browser.element('#message-input');
+    messageInput.setValue('test');
+    browser.click('#send-button');
+    var message = browser.element('.message');
+    assert.isTrue(browser.isExisting(message));
+  });
 });
 
 
