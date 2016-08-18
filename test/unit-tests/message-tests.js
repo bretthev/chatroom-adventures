83606bee9f481e5fc447d6b49c914f0ac55338d3
Brett Hevia <brett.hevia@gmail.com>
2016-08-18 08:56:59 -0600

    add oo unit test

--- test/unit-tests/message-tests.js
+++ test/unit-tests/message-tests.js
@@ -5,12 +5,12 @@ const Message = require('../../lib/message')
 describe('Message', function() {
   it('Message should be a function', function() {
     assert.isFunction(Message);
-  })
+  });
 
-  it('each message should have an id, title, and content property', function() {
-    var message = new Message({id: 1, title: "Brett's Idea", content: "Work somewhere with a devops team"})
+  it('each message should have an id, title, and user', function() {
+    var message = new Message({id: 1, user: 'Brett', content: 'Work somewhere with a devops team'})
 
-    assert.equal(message.id, 1, message.title, "Brett's Idea", message.content, "Work somewhere with a devops team");
-  })
+    assert.equal(message.id, 1, message.title, 'Brett Idea', message.content, 'Work on a devops team');
+  });
 
 })
